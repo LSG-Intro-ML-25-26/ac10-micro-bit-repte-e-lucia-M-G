ec5a34e12f959eaba8a5812e179b43303148b5bb
Lucía Martínez <lucia.martinez@gracia.lasalle.cat>
2025-12-03 17:58:20 +0100

    Funcionalidades de los botones

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-
+basic.show_string("REPTE 6-7")
+modo = 0
+basic.show_icon(IconNames.HEART)
+def on_button_pressed_a():
+    if modo == 0:
+        modo = 1
+        basic.show_string("G")
+    else:
+        modo = 0
+        basic.show_string("T")
+    basic.pause(500)
+    basic.clear_screen()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+def on_button_pressed_b():
+    if modo == 1:
+        x = 2
+        y = 2
+        basic.show_icon(IconNames.HAPPY)
+        basic.pause(300)
+input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -1,1 +1,27 @@
-
+basic.showString("REPTE 6-7")
+let modo = 0
+basic.showIcon(IconNames.Heart)
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    let modo: number;
+    if (modo == 0) {
+        modo = 1
+        basic.showString("G")
+    } else {
+        modo = 0
+        basic.showString("T")
+    }
+    
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    let x: number;
+    let y: number;
+    if (modo == 1) {
+        x = 2
+        y = 2
+        basic.showIcon(IconNames.Happy)
+        basic.pause(300)
+    }
+    
+})
